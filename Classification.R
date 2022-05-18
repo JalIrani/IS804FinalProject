9d74875d9c3cd5ecb90c1d4a40df3f81c40b7565
Jal Irani <jal@Jals-MacBook-Pro.local>
2022-05-18 19:10:06 -0400

    classification

--- Classification.R
+++ Classification.R
@@ -24,34 +24,18 @@ glm.pred
 
 
 # Now splitting the data into training and test sets
-#train=(Year<2005)
-#train
-#Smarket.2005=Smarket[!train,]
-#Smarket.2005
-#dim(Smarket.2005)
-#Direction.2005=Direction[!train]
+train=(Age<30)
+train
+playerStats=playerStats[!train,]
+playerStats.30
+dim(playerStats.30)
+playerStats.30.30=playerStats.30[!train]
 #Direction.2005
-#glm.fit=glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume,data=Smarket,family=binomial,subset=train)
-#glm.probs=predict(glm.fit,Smarket.2005,type="response")
-#glm.pred=rep("Down",252)
-#glm.pred[glm.probs>.5]="Up"
-#table(glm.pred,Direction.2005)
-#mean(glm.pred==Direction.2005)
-#mean(glm.pred!=Direction.2005)
-#glm.fit=glm(Direction~Lag1+Lag2,data=Smarket,family=binomial,subset=train)
-glm.probs=predict(glm.fit,Smarket.2005,type="response")
+glm.fit=glm(MVP~G+GS+MP+FG+FGA+FGP+3P+3PA+3PP+2P+2PA+2PP+eFGP+FG+FTA+FTP,data=playerStats,family=binomial,subset=train)
+glm.probs=predict(glm.fit,playerStats.30,type="response")
 glm.pred=rep("Down",252)
 glm.pred[glm.probs>.5]="Up"
-table(glm.pred,Direction.2005)
-mean(glm.pred==Direction.2005)
-#compute precision when predict "up"
-106/(106+76)
 #build a new dataset with two testing instances
 glm.probs=predict(glm.fit,newdata=data.frame(Lag1=c(1.2, 1.5),Lag2=c(1.1, -0.8)),type="response")
 glm.probs
-
-
-
-
-
 
